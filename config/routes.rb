db5bd452e7339f8d223ac5f98403b62f27c13bbe
kolawole akinmosin <kolawoleakinmosin01@gmail.com>
2023-03-09 15:06:37 +0000

    movie

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: "lists#home"
+
+  resources :lists do
+    resources :bookmarks
+  end
 end
